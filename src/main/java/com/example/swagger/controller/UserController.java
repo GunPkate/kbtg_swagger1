77ec0a38ec4927ed3b8787ab88885a7d049d1d5b
GunPKate <gunPK>
2024-01-20 20:34:37 +0700

    add swagger ui dependency

--- src/main/java/com/example/swagger/controller/UserController.java
+++ src/main/java/com/example/swagger/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.swagger.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -13,5 +14,11 @@ public class UserController {
         @GetMapping
         public  ResponseEntity<List<String>> listUser(){
             return new ResponseEntity<>( List.of("aa","bb"), HttpStatus.OK);
+        }
+
+        @PostMapping
+//        @ApiResponse(responseCode = "201",description = "User created by GP")
+        public  ResponseEntity<Integer> createUser(Integer id,String name){
+            return new ResponseEntity<>(id,HttpStatus.CREATED);
         }
 }
